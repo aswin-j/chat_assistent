395dbf40701606271c06c25fdcb5b1657d0f9e7b
aswin joseph <90128929+aswin-j@users.noreply.github.com>
2024-03-15 21:54:20 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -7,3 +7,16 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
+from langchain.document_loaders import PyPDFLoader
+
+# Load PDF
+loaders = [
+    # Duplicate documents on purpose - messy data
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf"),
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf"),
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture02.pdf"),
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture03.pdf")
+]
+docs = []
+for loader in loaders:
+    docs.extend(loader.load())
